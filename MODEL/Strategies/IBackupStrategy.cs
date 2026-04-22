6a79be816ba59b27fa6cd78d05fafa34db4e8dfb
TimotheeVCesi <timothee.viriot@viacesi.fr>
2026-04-22 10:03:12 +0200

    feat: add Backup Strategy

---
+++ MODEL/Strategies/IBackupStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasySave.Model.Strategies
+{
+    public interface IBackupStrategy
+    {
+        /// <summary>
+        /// Executes the backup logic for the given job.
+        /// </summary>
+        void Execute(BackupJob job);
+    }
+}
